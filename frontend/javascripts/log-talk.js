f198ef6fd000d4e91d2c1663745af53a7676a5cd
Riku Takeuchi <rikuson@users.noreply.github.com>
2023-11-25 14:31:09 +0900

    feat: add frontend

---
+++ frontend/javascripts/log-talk.js
@@ -0,0 +1,97 @@
+const BROWSER_COLOR = {
+  red: "#BD2130",
+  yellow: "#D39E00",
+  green: "#1E7E34",
+  blue: "#117A8B",
+  default: "inherit",
+};
+
+const highlight = (label, color) => {
+  return [`%c${label}`, `color: ${BROWSER_COLOR[color]}`];
+};
+
+class LogTalk {
+  static get ERR_INVALID_ARGUMENT() {
+    return "Invalid argument";
+  }
+
+  static get ERR_METHOD_NAME_RESERVED() {
+    return "Method name is reserved";
+  }
+
+  static get LOGGING_METHODS() {
+    return [
+      {
+        name: "debug",
+        label: "DEBUG",
+        level: 1,
+        color: "default",
+        output: console.log,
+      },
+      {
+        name: "info",
+        label: "INFO",
+        level: 2,
+        color: "blue",
+        output: console.log,
+      },
+      {
+        name: "success",
+        label: "SUCCESS",
+        level: 3,
+        color: "green",
+        output: console.log,
+      },
+      {
+        name: "warn",
+        label: "WARN",
+        level: 4,
+        color: "yellow",
+        output: console.warn || console.log,
+      },
+      {
+        name: "error",
+        label: "ERROR",
+        level: 5,
+        color: "red",
+        output: console.error || console.log,
+      },
+    ];
+  };
+
+  static get DEFAULT_LOGGING_METHOD() {
+    return {
+      level: 1,
+      color: "default",
+      output: console.log,
+      timeFormat: "[[]YYYY-MM-DD HH:mm:ss]",
+    };
+  };
+
+  constructor(level = 1, option = {}) {
+    this.__option = { ...LogTalk.DEFAULT_LOGGING_METHOD, ...option };
+    this.__level = level;
+    LogTalk.LOGGING_METHODS.forEach(this.setMethod.bind(this));
+  }
+
+  setMethod(method) {
+    if (typeof method !== "object") {
+      throw new Error(LogTalk.ERR_INVALID_ARGUMENT);
+    }
+    method = { ...this.__option, ...method };
+    const { name, label, level, timeFormat, output, color } = method;
+    switch (name) {
+      case "setMethod":
+        throw new Error(LogTalk.ERR_METHOD_NAME_RESERVED);
+    }
+    if (typeof console === 'undefined') return false;
+    if (!this.__level || level < this.__level) {
+      this[name] = () => {};
+      return false;
+    }
+    const args = highlight(typeof label !== "undefined" ? label : name, color);
+    this[name] = output.bind(console, ...args);
+  }
+}
+
+export default LogTalk;
---
+++ frontend/javascripts/room.js
@@ -0,0 +1,61 @@
+import { io } from 'https://cdn.socket.io/4.7.2/socket.io.esm.min.js';
+import LogTalk from './log-talk.js';
+
+const logger = new LogTalk();
+const publishing = document.getElementById('publishing');
+const subscribing = document.getElementById('subscribing');
+
+const { searchParams } = new URL(document.location);
+
+const query = {
+  roomId: searchParams.get('roomId'),
+  userName: searchParams.get('userName'),
+};
+const socket = io({ query, transports: ['websocket'] });
+logger.info('Try to connect socket', query);
+
+socket.on('connect', () => {
+  logger.info('Socket is connected', {
+    recovered: socket.recovered,
+  });
+});
+socket.on('create-room', (data) => {
+  const { roomId } =  data;
+  logger.info(`Room ${roomId} was created`);
+});
+socket.on('delete-room', (data) => {
+  const { roomId } =  data;
+  logger.info(`Room ${roomId} was deleted`);
+});
+socket.on('join-room', (data) => {
+  const { id, roomId, messages } = data;
+  logger.info(`Socket ${id} has joined room ${roomId}`, data);
+  const nodes = messages.forEach(({ userName, message }) => renderMessage(userName, message));
+});
+socket.on('message', (data) => {
+  const { userName, message } = data;
+  logger.info(`Receive ${userName}'s message "${message}"`);
+  renderMessage(userName, message);
+});
+
+function onSubmitMessage(e) {
+  event.preventDefault();
+  const data = { message: publishing.value }
+  socket.emit('message', data);
+  logger.info('Send message', 'message');
+  publishing.value = '';
+}
+
+function renderMessage(userName, message) {
+  const div = document.createElement('div');
+  div.className = 'p-2 border-b overflow-hidden leading-loose';
+  div.append(`${userName}: ${message}`);
+  subscribing.append(div);
+}
+
+function timeString(date) {
+  const ms = ('00' + date.getMilliseconds()).slice(-3);
+  return `${date.toLocaleString('ja-JP')}.${ms}`;
+}
+
+window.onSubmitMessage = onSubmitMessage;
